7ae253e8144f76a22cfe62e6691cea51eff8eea3
nadobrranichh <nazarshch26@gmail.com>
2026-05-27 18:39:34 +0300

    fix: fixed index file to serve with CORS

--- index.ts
+++ index.ts
@@ -8,8 +8,14 @@ import { registerSocketHandlers } from "./sockets/index.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 
 const server = http.createServer(app);
 
@@ -17,6 +23,7 @@ export const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
