7bd6f21d80cda94b468b5ef037c2d166220d8254
nadobrranichh <nazarshch26@gmail.com>
2026-05-27 14:50:40 +0300

    fix: fixed set-name event to be a socket event

--- sockets/names.socket.ts
+++ sockets/names.socket.ts
@@ -2,7 +2,7 @@ import type { Server, Socket } from "socket.io";
 import { users } from "../store/store.js";
 
 export function setupNameSettingSockets(io: Server, socket: Socket) {
-  io.on("set-name", (name: string) => {
+  socket.on("set-name", (name: string) => {
     const user = users.get(socket.id);
     if (user) user.name = name;
   });
